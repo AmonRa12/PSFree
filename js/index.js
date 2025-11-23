36f4f66e1a9d689534b7fe774a1b9d0e4f36e529
AmonRa12 <gph5171@gmail.com>
2025-11-23 21:02:04 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -255,17 +255,17 @@ function choosejb(hen) {
 }
 
 function showpayloads() {
-  if (document.getElementById('payloadsbtn').textContent == 'Payloads') {
+  if (document.getElementById('payloadsbtn').textContent == 'حمولات') {
   document.getElementById('jailbreak-page').style.display = 'none';
   document.getElementById('PS4FW').style.display = 'none';
   document.getElementById('payloads-page').style.display = 'block';
-  document.getElementById('payloadsbtn').textContent = 'Jailbreak';
+  document.getElementById('payloadsbtn').textContent = 'كسر الحماية';
   localStorage.setItem('visibleDiv', 'payloads-page');
   }else{
   document.getElementById('jailbreak-page').style.display = 'block';
   document.getElementById('PS4FW').style.display = 'flex';
   document.getElementById('payloads-page').style.display = 'none';
-  document.getElementById('payloadsbtn').textContent = 'Payloads';
+  document.getElementById('payloadsbtn').textContent = 'حمولات';
   localStorage.setItem('visibleDiv', 'jailbreak-page');
 
   };
